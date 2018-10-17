4bff95263663443357b1f633036b89e93b4fe66a
AdrienPoupa <adrien@poupa.fr>
2018-10-17 17:11:32 -0400

    Overcatch and System.exit

--- src/main/java/ca/concordia/soen7481/assignment/Main.java
+++ src/main/java/ca/concordia/soen7481/assignment/Main.java
@@ -1,17 +1,7 @@
 package ca.concordia.soen7481.assignment;
 
-import ca.concordia.soen7481.assignment.checkers.Checker;
-import ca.concordia.soen7481.assignment.checkers.EqualsHashcode;
-import ca.concordia.soen7481.assignment.checkers.StringComparison;
-import ca.concordia.soen7481.assignment.checkers.TodoFixMeCatchBlock;
+import ca.concordia.soen7481.assignment.checkers.*;
 import ca.concordia.soen7481.assignment.visitors.*;
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.resolution.types.ResolvedType;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
 import java.io.File;
 
@@ -25,27 +15,6 @@ public class Main {
 
     static void initSetup()
     {
-    	// Set up a minimal type solver that only looks at the classes used to run this sample.
-        CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
-        combinedTypeSolver.add(new ReflectionTypeSolver());
-
-        // Configure JavaParser to use type resolution
-        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
-        JavaParser.getStaticConfiguration().setSymbolResolver(symbolSolver);
-
-        // Parse some code
-        CompilationUnit cu = JavaParser.parse("class X { int x() { return 1 + 1.0 - 5; } }");
-
-        // Find all the calculations with two sides:
-        System.out.println("Find calculations");
-        cu.findAll(BinaryExpr.class).forEach(be -> {
-            // Find out what type it has:
-            ResolvedType resolvedType = be.calculateResolvedType();
-
-            // Show that it's "double" in every case:
-            System.out.println(be.toString() + " is a: " + resolvedType);
-        });
-
         Visitor visitor;
 
         // List classes
@@ -87,5 +56,8 @@ public class Main {
         checker = new StringComparison();
         checker.check(projectDir);
 
+        // Overcatch and System.exit
+        checker = new OvercatchExceptionTermination();
+        checker.check(projectDir);
     }
 }
